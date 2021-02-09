a1dd9d0b82f7fee513a1ad374266a3229d7e440d
ludivine <ludivine.seve@epsi.fr>
2021-02-09 14:32:36 +0100

    init 2

---
+++ docs/precache-manifest.6cd04eacab97927f632220ca3b4d35f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3965f3a65ad0c97a2ef",
+    "url": "/vue-app-2/css/app.e2406e29.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b23c8e836a0e5244a632bd25b3dc48c3",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "c3965f3a65ad0c97a2ef",
+    "url": "/vue-app-2/js/app.708097e5.js"
+  },
+  {
+    "revision": "3a6798fe18ab96b859e8",
+    "url": "/vue-app-2/js/chunk-vendors.ca792762.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
