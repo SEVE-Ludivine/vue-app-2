a08e5fdb1821ecbd4e95f43005c519f8bfacee07
ludivine <ludivine.seve@epsi.fr>
2021-03-09 12:02:48 +0100

    Liste des parcs de Nantes

---
+++ src/components/Parc.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">{{ product.fields.nom_complet }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ product.fields.adresse }}, {{ product.fields.code_postal }} {{ product.fields.commune }}</h6>
+        <p class="card-text">Accès par les transports en communs : {{ product.fields.acces_transport_commun }}</p>
+        <div class="card-text" v-if="chien==='NON'">Les chiens ne sont pas autorisés dans ce parc.</div>
+        <div class="card-text" v-else>Les chiens sont autorisés dans ce parc.</div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const apiURL = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique'
+export default {
+  name: "Parc",
+  props : ["obj","src"],
+  data() {
+    return {
+      product : {},
+      errors: "",
+      error : false,
+      chien: null
+    }
+  },
+  created: function() {
+    this.product = this.obj;
+    this.chien= this.product.fields.chiens_autorises
+    console.log(apiURL);
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/views/Liste.vue
+++ src/views/Liste.vue
@@ -1,54 +1,15 @@
-<!--<template>-->
-<!--  <div class="liste">-->
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-<!--    <span v-for="records in dataAPI">-->
-<!--      <p>{{records.datasetid}}</p>-->
-<!--      <p>Vous êtes {{records.fields.commune}} {{records.rank}} {{records.leaguePoints}} LP</p>-->
-<!--      <p>Vous avez fait {{records.wins}} TOP 1.</p>-->
-<!--    </span>-->
-<!--  </div>-->
-<!--</template>-->
-<!--<script src="https://unpkg.com/axios/dist/axios.min.js"></script>-->
-<!--<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>-->
-<!--<script>-->
-<!--// @ is an alias to /src-->
-<!--import HelloWorld from '@/components/HelloWorld.vue'-->
-<!--const apiURL = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique'-->
 
-<!--	const demo = new Vue({-->
-<!--		el: '#demo',-->
-<!--		data: {-->
-<!--      dataAPI: []-->
-<!--    },-->
-<!--		methods: {-->
-<!--      fetchData2: async function (pseudo) {-->
-<!--          const dataAPI= await axios.get(apiURL)-->
-<!--          this.dataAPI = dataAPI.data-->
-<!--          console.log(this.dataAPI)-->
-<!--          console.log(this.dataAPI.records)-->
-<!--          console.log(this.dataAPI.records.datasetid)-->
-<!--      }-->
-<!--		}-->
-<!--  })-->
-
-<!--export default {-->
-<!--  name: "Liste des données",-->
-<!--  components: {-->
-<!--    HelloWorld-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
 <template>
   <div class="container">
     <div class="row">
       <div class="col-lg-3">
         <h1 class="my-4">Liste des parcs de Nantes</h1>
         <HelloWorld msg="PARCS"/>
+        <!--<a target="_blank" href={{ product.fields.siteweb }}>{{ product.fields.siteweb }}</a>-->
       </div>
       <div class="col-lg-9">
         <div class="row">
-          <Product v-for="parc in this.json" :key="parc.id" :obj="parc" src="mag"></Product>
+          <Parc v-for="listeParc in this.json" :key="listeParc.recordid" :obj="listeParc" src="listeParc"></Parc>
         </div>
       </div>
     </div>
@@ -56,11 +17,12 @@
 </template>
 <script>
 import HelloWorld from "../components/HelloWorld";
+import Parc from "../components/Parc";
 import axios from "axios";
 const apiURL = 'https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique'
 export default {
-  name: "Kiosque",
-  components: {HelloWorld},
+  name: "Parcs",
+  components: {HelloWorld, Parc},
   created() {
     this.fetchData()
   },
@@ -74,8 +36,8 @@ export default {
       try {
         axios.get(apiURL)
             .then(res => {
-              this.json = (res.data)
-              console.log(res.data)
+              this.json = (res.data.records)
+              console.log(res.data.records)
             })
       } catch (error) {
         console.log(error);
